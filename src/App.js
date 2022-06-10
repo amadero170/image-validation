a71ccfaca651e12bddf7b393dc78df90a92112b4
amadero170 <84208214+amadero170@users.noreply.github.com>
2022-06-10 06:28:44 -0500

    boton que modifica estado local de Imagenes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,33 @@ import "./App.css";
 import Images from "./Images.js";
 import db from "./db.json";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { useState } from "react";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyALvtbcfz7xBpqTmbRwTZbwVaUAz6UsulY",
+  authDomain: "primer-proyecto-firebase-f46f5.firebaseapp.com",
+  projectId: "primer-proyecto-firebase-f46f5",
+  storageBucket: "primer-proyecto-firebase-f46f5.appspot.com",
+  messagingSenderId: "796584806312",
+  appId: "1:796584806312:web:6a7392614190e883ad3f9d",
+  measurementId: "G-ZSLHTLY13K",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const data_arr = db.db;
 
 function App() {
+  // const [status,setStatus] = useState('ok')
   return (
     <div className="App">
       {data_arr.map((item, i) => (
--- src/Images.js
+++ src/Images.js
@@ -1,15 +1,24 @@
 import React from "react";
+import { useState } from "react";
 
 export default function Images(props) {
+  const [status, setStatus] = useState("ok");
   return (
     <div>
       <div className="item-wrapper">
         <p>{props.index + 1}</p>
         <p>{props.name}</p>
-        <p>{props.title}</p>
+        <p className={status !== "ok" ? "bg" : null}>{props.title}</p>
         <div className="img-wrapper">
           <img src={props.img_url} alt={props.name} />
         </div>
+        <button
+          onClick={() =>
+            status === "ok" ? setStatus("corregir") : setStatus("ok")
+          }
+        >
+          Verificar
+        </button>
       </div>
     </div>
   );
